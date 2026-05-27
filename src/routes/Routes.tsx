cc3358f842eef46395914c0230aee9033ac91300
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-27 10:05:58 -0600

    feat(routes): archivo de rutas y ruta principal

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import { TrainingEvent } from "../pages/TrainingEvent/TrainingEvent";
+
+export const MyRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<TrainingEvent />} />
+    </Routes>
+  );
+};
