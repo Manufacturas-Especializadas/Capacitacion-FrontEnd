d525b132093c5fb77e1b077072ddd86ef647ac34
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-27 11:54:01 -0600

    feat(route): ruta principal

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { TrainingEvent } from "../pages/TrainingEvent/TrainingEvent";
+import { TrainingHistory } from "../pages/TrainingEvent/TrainingHistory";
 
 export const MyRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<TrainingHistory />} />
       <Route path="/lista-asistencia" element={<TrainingEvent />} />
     </Routes>
   );
