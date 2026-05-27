b20535e014c00ca4d2b3cdf53da2aaf463115b3c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-27 15:20:33 -0600

    feat(route): ruta del fomulario de registro

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { TrainingEvent } from "../pages/TrainingEvent/TrainingEvent";
 import { TrainingHistory } from "../pages/TrainingEvent/TrainingHistory";
+import { TrainingEventForm } from "../pages/TrainingEvent/TrainingEventForm";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<TrainingHistory />} />
+      <Route path="/registro-asistencia" element={<TrainingEventForm />} />
       <Route path="/lista-asistencia" element={<TrainingEvent />} />
     </Routes>
   );
