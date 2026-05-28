feeba3b82819b7c3599fa0c0a13b80cf0407969b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-28 07:05:50 -0600

    feat(route): ruta del registro de usuarios

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,12 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import { TrainingEvent } from "../pages/TrainingEvent/TrainingEvent";
 import { TrainingHistory } from "../pages/TrainingEvent/TrainingHistory";
 import { TrainingEventForm } from "../pages/TrainingEvent/TrainingEventForm";
+import { EnrollmentMatrix } from "../components/TrainingEventUI/EnrollmentMatrix";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<TrainingHistory />} />
       <Route path="/registro-asistencia" element={<TrainingEventForm />} />
+      <Route
+        path="/registro-asistencia/usuarios"
+        element={<EnrollmentMatrix />}
+      />
       <Route path="/lista-asistencia" element={<TrainingEvent />} />
     </Routes>
   );
