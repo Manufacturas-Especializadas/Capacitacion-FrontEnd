b4d79fd3b51b2d60ab6d95a417b799bc542b6b2a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 07:55:58 -0600

    feat(app): toast global

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { MyRoutes } from "./routes/Routes";
+import { Toaster } from "sonner";
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <Toaster richColors position="top-right" />
       <Navbar />
       <main>
         <MyRoutes />
