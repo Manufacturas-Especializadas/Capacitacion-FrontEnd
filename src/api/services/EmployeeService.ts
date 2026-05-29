943ebdb54754aa927b17b765110151cdb80cccea
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 10:59:54 -0600

    feat(service): metodo para el registro de nuevos usuarios

--- src/api/services/EmployeeService.ts
+++ src/api/services/EmployeeService.ts
@@ -1,12 +1,17 @@
 import { API_CONFIG } from "../../config/api";
-import type { Employee } from "../../types/Types";
+import type { CreateEmployee, Employee } from "../../types/Types";
 import { apiClient } from "../client";
 
 class EmployeeService {
   private getAllEmployeeEndpoint = API_CONFIG.endpoint.employees.allEmployees;
+  private createEmployeeEndpoint = API_CONFIG.endpoint.employees.createEmployee;
 
   async getAllEmployee(): Promise<Employee[]> {
     return apiClient.get<Employee[]>(this.getAllEmployeeEndpoint);
+  }
+
+  async createEmployee(data: CreateEmployee): Promise<void> {
+    return apiClient.post<void>(this.createEmployeeEndpoint, data);
   }
 }
 
