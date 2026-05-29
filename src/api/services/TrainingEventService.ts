24180b975276808f2ff5adbd4ab61dd4fa1e7c62
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 07:24:53 -0600

    feat(service): metodo para registrar un nuevo evento

--- src/api/services/TrainingEventService.ts
+++ src/api/services/TrainingEventService.ts
@@ -1,3 +1,13 @@
-class TrainingEventService {}
+import { API_CONFIG } from "../../config/api";
+import type { CreateTrainingEvent } from "../../types/Types";
+import { apiClient } from "../client";
+
+class TrainingEventService {
+  private createEventEndpoint = API_CONFIG.endpoint.trainingEvent.createEvent;
+
+  async createEvent(data: CreateTrainingEvent): Promise<void> {
+    return apiClient.post<void>(this.createEventEndpoint, data);
+  }
+}
 
 export const trainingEventService = new TrainingEventService();
