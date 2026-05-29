6b982e83368cec116881f474347991963140c253
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 07:31:02 -0600

    feat(service): metodo para la asignacion de usuarios a eventos

--- src/api/services/TrainingEventService.ts
+++ src/api/services/TrainingEventService.ts
@@ -1,12 +1,18 @@
 import { API_CONFIG } from "../../config/api";
-import type { CreateTrainingEvent } from "../../types/Types";
+import type { AssignAttendees, CreateTrainingEvent } from "../../types/Types";
 import { apiClient } from "../client";
 
 class TrainingEventService {
   private createEventEndpoint = API_CONFIG.endpoint.trainingEvent.createEvent;
+  private assignAttendeesEndpoint =
+    API_CONFIG.endpoint.trainingEvent.assingAttendees;
 
   async createEvent(data: CreateTrainingEvent): Promise<void> {
     return apiClient.post<void>(this.createEventEndpoint, data);
+  }
+
+  async assignAttendees(data: AssignAttendees): Promise<void> {
+    return apiClient.post<void>(this.assignAttendeesEndpoint, data);
   }
 }
 
