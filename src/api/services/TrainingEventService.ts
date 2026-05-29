d64107c028e26ca6986a800d4fce6d72094d5456
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 07:34:22 -0600

    feat(service): metodo para guardar la asistencia y las calificaciones de los usuarios

--- src/api/services/TrainingEventService.ts
+++ src/api/services/TrainingEventService.ts
@@ -1,11 +1,17 @@
 import { API_CONFIG } from "../../config/api";
-import type { AssignAttendees, CreateTrainingEvent } from "../../types/Types";
+import type {
+  AssignAttendees,
+  CreateTrainingEvent,
+  SaveAttendance,
+} from "../../types/Types";
 import { apiClient } from "../client";
 
 class TrainingEventService {
   private createEventEndpoint = API_CONFIG.endpoint.trainingEvent.createEvent;
   private assignAttendeesEndpoint =
     API_CONFIG.endpoint.trainingEvent.assingAttendees;
+  private saveAttendanceEndpoint =
+    API_CONFIG.endpoint.trainingEvent.saveAttendance;
 
   async createEvent(data: CreateTrainingEvent): Promise<void> {
     return apiClient.post<void>(this.createEventEndpoint, data);
@@ -13,6 +19,10 @@ class TrainingEventService {
 
   async assignAttendees(data: AssignAttendees): Promise<void> {
     return apiClient.post<void>(this.assignAttendeesEndpoint, data);
+  }
+
+  async saveAttendance(data: SaveAttendance): Promise<void> {
+    return apiClient.post<void>(this.saveAttendanceEndpoint, data);
   }
 }
 
