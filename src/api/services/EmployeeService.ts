abfbc059bd4be1dc8b19f701311372f197a3f14b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 10:55:24 -0600

    feat(service): metodo que obtiene todos los empleados

--- src/api/services/EmployeeService.ts
+++ src/api/services/EmployeeService.ts
@@ -1,3 +1,13 @@
-class EmployeeService {}
+import { API_CONFIG } from "../../config/api";
+import type { Employee } from "../../types/Types";
+import { apiClient } from "../client";
+
+class EmployeeService {
+  private getAllEmployeeEndpoint = API_CONFIG.endpoint.employees.allEmployees;
+
+  async getAllEmployee(): Promise<Employee[]> {
+    return apiClient.get<Employee[]>(this.getAllEmployeeEndpoint);
+  }
+}
 
 export const employeeService = new EmployeeService();
