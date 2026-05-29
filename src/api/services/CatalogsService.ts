5d4f5d71d433dd3e05c9d47725cf1ca65978f06d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 09:31:52 -0600

    feat(service): metodo para obtener las salas de entrenamiento

--- src/api/services/CatalogsService.ts
+++ src/api/services/CatalogsService.ts
@@ -1,3 +1,13 @@
-class CatalogsService {}
+import { API_CONFIG } from "../../config/api";
+import type { TrainingRooms } from "../../types/Types";
+import { apiClient } from "../client";
+
+class CatalogsService {
+  private getRoomsEndpoint = API_CONFIG.endpoint.catalgos.rooms;
+
+  async getRooms(): Promise<TrainingRooms[]> {
+    return apiClient.get<TrainingRooms[]>(this.getRoomsEndpoint);
+  }
+}
 
 export const catalogsService = new CatalogsService();
