45758a2240460def3faa9d0dac84bccbf256d906
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-29 09:33:20 -0600

    feat(service): metodo para obtener las lineas de produccion

--- src/api/services/CatalogsService.ts
+++ src/api/services/CatalogsService.ts
@@ -1,12 +1,17 @@
 import { API_CONFIG } from "../../config/api";
-import type { TrainingRooms } from "../../types/Types";
+import type { ProductionLines, TrainingRooms } from "../../types/Types";
 import { apiClient } from "../client";
 
 class CatalogsService {
   private getRoomsEndpoint = API_CONFIG.endpoint.catalgos.rooms;
+  private getProductionLinesEndpoint = API_CONFIG.endpoint.catalgos.lines;
 
   async getRooms(): Promise<TrainingRooms[]> {
     return apiClient.get<TrainingRooms[]>(this.getRoomsEndpoint);
+  }
+
+  async getProductionLines(): Promise<ProductionLines[]> {
+    return apiClient.get<ProductionLines[]>(this.getProductionLinesEndpoint);
   }
 }
 
