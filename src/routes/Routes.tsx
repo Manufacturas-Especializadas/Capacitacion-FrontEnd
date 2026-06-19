bb027021413b9e7b44496a71a2a219b721c317bb
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-19 15:25:59 -0600

    refactor(route): componente rutas ordenado

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import { TrainingEvent } from "../pages/TrainingEvent/TrainingEvent";
+import { Home } from "../pages/Home/Home";
 import { TrainingHistory } from "../pages/TrainingEvent/TrainingHistory";
 import { TrainingEventForm } from "../pages/TrainingEvent/TrainingEventForm";
 import { EnrollmentMatrix } from "../components/TrainingEventUI/EnrollmentMatrix";
-import { Home } from "../pages/Home/Home";
-import { WeldersChecklistForm } from "../pages/WeldersChecklist/WeldersChecklistForm";
+import { TrainingEvent } from "../pages/TrainingEvent/TrainingEvent";
 import { WeldersEvaluationsDashboard } from "../pages/WeldersChecklist/WeldersEvaluationsDashboard";
+import { WeldersChecklistForm } from "../pages/WeldersChecklist/WeldersChecklistForm";
 import { WeldersEvaluationDetails } from "../components/WeldersChecklistUI/WeldersEvaluationDetails";
 import { WeldersEditEvaluation } from "../components/WeldersChecklistUI/WeldersEditEvaluation";
 
@@ -13,6 +13,7 @@ export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
       <Route
         path="/historial-registro-de-asistencia"
         element={<TrainingHistory />}
@@ -31,14 +32,12 @@ export const MyRoutes = () => {
         path="/historial-checklist-soldadores"
         element={<WeldersEvaluationsDashboard />}
       />
-
-      <Route path="/ver/:id" element={<WeldersEvaluationDetails />} />
-
       <Route
         path="/registro-de-checklist-soldadores"
         element={<WeldersChecklistForm />}
       />
 
+      <Route path="/ver/:id" element={<WeldersEvaluationDetails />} />
       <Route path="/editar/:id" element={<WeldersEditEvaluation />} />
     </Routes>
   );
