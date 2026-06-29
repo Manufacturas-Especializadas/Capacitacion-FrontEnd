e99f7c3f6bc7546ff2a486e2d278854f484fa302
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-29 09:19:00 -0600

    feat(service): metodo para obtener la informacion del empleado por Id

--- src/api/services/EmployeeService.ts
+++ src/api/services/EmployeeService.ts
@@ -4,11 +4,17 @@ import { apiClient } from "../client";
 
 class EmployeeService {
   private getAllEmployeeEndpoint = API_CONFIG.endpoint.employees.allEmployees;
+  private getEmployeeByIdEndpoint =
+    API_CONFIG.endpoint.employees.getEmployeeById;
   private createEmployeeEndpoint = API_CONFIG.endpoint.employees.createEmployee;
   private updateEmployeeEndpoint = API_CONFIG.endpoint.employees.updateEmployee;
 
   async getAllEmployee(): Promise<Employee[]> {
     return apiClient.get<Employee[]>(this.getAllEmployeeEndpoint);
+  }
+
+  async getEmployeeById(id: number): Promise<Employee> {
+    return apiClient.get<Employee>(`${this.getEmployeeByIdEndpoint}${id}`);
   }
 
   async createEmployee(data: CreateEmployee): Promise<Employee> {
