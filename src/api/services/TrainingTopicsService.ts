884108e3bb0edf62bd3ae33ea1b220dba2cc51d3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-30 10:53:23 -0600

    feat(service): metodo para obtener todos los temas de entrenamientos

--- src/api/services/TrainingTopicsService.ts
+++ src/api/services/TrainingTopicsService.ts
@@ -1,11 +1,19 @@
 import { API_CONFIG } from "../../config/api";
-import type { CreateTrainingTopics } from "../../types/Types";
+import type {
+  CreateTrainingTopics,
+  TrainingTopicsAll,
+} from "../../types/Types";
 import { apiClient } from "../client";
 
 class TrainingTopicsService {
+  private getAllEndpoint = API_CONFIG.endpoint.trainingTopics.all;
   private createEndpoint = API_CONFIG.endpoint.trainingTopics.create;
   private updateEndpoint = API_CONFIG.endpoint.trainingTopics.update;
   private deleteEndpoint = API_CONFIG.endpoint.trainingTopics.delete;
+
+  async getAll(): Promise<TrainingTopicsAll[]> {
+    return apiClient.get<TrainingTopicsAll[]>(this.getAllEndpoint);
+  }
 
   async create(data: CreateTrainingTopics): Promise<any> {
     return apiClient.post<any>(this.createEndpoint, data);
