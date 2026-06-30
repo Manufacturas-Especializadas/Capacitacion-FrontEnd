4caf1ee11ccdd129f2d540605b67ee89e3955e3a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-30 10:21:31 -0600

    feat(service): metodo delete - temas de entrenamientos

--- src/api/services/TrainingTopicsService.ts
+++ src/api/services/TrainingTopicsService.ts
@@ -5,6 +5,7 @@ import { apiClient } from "../client";
 class TrainingTopicsService {
   private createEndpoint = API_CONFIG.endpoint.trainingTopics.create;
   private updateEndpoint = API_CONFIG.endpoint.trainingTopics.update;
+  private deleteEndpoint = API_CONFIG.endpoint.trainingTopics.delete;
 
   async create(data: CreateTrainingTopics): Promise<any> {
     return apiClient.post<any>(this.createEndpoint, data);
@@ -12,6 +13,10 @@ class TrainingTopicsService {
 
   async update(id: number, data: CreateTrainingTopics): Promise<any> {
     return apiClient.put<any>(`${this.updateEndpoint}${id}`, data);
+  }
+
+  async delete(id: number): Promise<any> {
+    return apiClient.delete<any>(`${this.deleteEndpoint}${id}`);
   }
 }
 
