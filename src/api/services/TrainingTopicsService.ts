3a35d625344a5012f222d05a8abb70131508e03a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-30 10:19:55 -0600

    feat(service): metodo update - temas de entrenamientos

--- src/api/services/TrainingTopicsService.ts
+++ src/api/services/TrainingTopicsService.ts
@@ -4,9 +4,14 @@ import { apiClient } from "../client";
 
 class TrainingTopicsService {
   private createEndpoint = API_CONFIG.endpoint.trainingTopics.create;
+  private updateEndpoint = API_CONFIG.endpoint.trainingTopics.update;
 
   async create(data: CreateTrainingTopics): Promise<any> {
     return apiClient.post<any>(this.createEndpoint, data);
+  }
+
+  async update(id: number, data: CreateTrainingTopics): Promise<any> {
+    return apiClient.put<any>(`${this.updateEndpoint}${id}`, data);
   }
 }
 
