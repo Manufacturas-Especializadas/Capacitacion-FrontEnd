e7bfdc6d2fac323a20a9e796f451f44e5c8fd04e
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-30 10:19:09 -0600

    feat(service): metodo create - temas de entrenamientos

--- src/api/services/TrainingTopicsService.ts
+++ src/api/services/TrainingTopicsService.ts
@@ -1,3 +1,13 @@
-class TrainingTopicsService {}
+import { API_CONFIG } from "../../config/api";
+import type { CreateTrainingTopics } from "../../types/Types";
+import { apiClient } from "../client";
+
+class TrainingTopicsService {
+  private createEndpoint = API_CONFIG.endpoint.trainingTopics.create;
+
+  async create(data: CreateTrainingTopics): Promise<any> {
+    return apiClient.post<any>(this.createEndpoint, data);
+  }
+}
 
 export const trainingTopicsService = new TrainingTopicsService();
