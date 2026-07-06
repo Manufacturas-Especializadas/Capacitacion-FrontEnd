f8acd6db820c7746b1b8604b893e3709a8fbd782
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-06 12:06:32 -0600

    feat(component): url del formulario de reportes de entrenamiento

--- src/components/UI/TrainingReportsUi/TrainingReportsHeader.tsx
+++ src/components/UI/TrainingReportsUi/TrainingReportsHeader.tsx
@@ -55,7 +55,7 @@ export const TrainingReportsHeader = ({
             Temas de entrenamientos
           </button>
           <button
-            onClick={() => navigate("/reporte-entrenamiento/nuevo")}
+            onClick={() => navigate("/reportes-entrenaminetos/nuevo")}
             className="w-full md:w-auto flex items-center justify-center gap-2 px-5 py-3 
             bg-blue-600 hover:bg-blue-700 text-white text-sm font-bold rounded-xl 
             shadow-sm transition-all active:scale-95 cursor-pointer"
