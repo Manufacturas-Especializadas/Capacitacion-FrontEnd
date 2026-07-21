2402f0816103a1655aca717c1cbd3b01f8bf5fda
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-21 10:09:13 -0600

    feat(service): metodo para obtener la lista de tutores

--- src/api/services/CatalogsService.ts
+++ src/api/services/CatalogsService.ts
@@ -1,10 +1,11 @@
 import { API_CONFIG } from "../../config/api";
-import type { ProductionLines, TrainingRooms } from "../../types/Types";
+import type { ProductionLines, TrainingRooms, Tutors } from "../../types/Types";
 import { apiClient } from "../client";
 
 class CatalogsService {
   private getRoomsEndpoint = API_CONFIG.endpoint.catalgos.rooms;
   private getProductionLinesEndpoint = API_CONFIG.endpoint.catalgos.lines;
+  private getTutorsEndpoint = API_CONFIG.endpoint.catalgos.tutors;
 
   async getRooms(): Promise<TrainingRooms[]> {
     return apiClient.get<TrainingRooms[]>(this.getRoomsEndpoint);
@@ -12,6 +13,10 @@ class CatalogsService {
 
   async getProductionLines(): Promise<ProductionLines[]> {
     return apiClient.get<ProductionLines[]>(this.getProductionLinesEndpoint);
+  }
+
+  async getTutors(): Promise<Tutors[]> {
+    return apiClient.get<Tutors[]>(this.getTutorsEndpoint);
   }
 }
 
