beda3144bcc336bc45f44e1d940992f0bacc3c0f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-21 10:21:03 -0600

    feat(types): types preguntas del formulario

--- src/api/services/TutoringProgram.ts
+++ src/api/services/TutoringProgram.ts
@@ -1,3 +1,14 @@
-class TutoringProgram {}
+import { API_CONFIG } from "../../config/api";
+import type { Form } from "../../types/Types";
+import { apiClient } from "../client";
+
+class TutoringProgram {
+  private getFormQuestionsEndpoint =
+    API_CONFIG.endpoint.tutoringProgram.getFormQuestion;
+
+  async getFormQuestion(): Promise<Form[]> {
+    return apiClient.get<Form[]>(this.getFormQuestionsEndpoint);
+  }
+}
 
 export const tutoringProgram = new TutoringProgram();
