d1bd85d3555e83b26357d426f136206b211319c7
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-22 08:01:19 -0600

    feat(service): metodo post para el registro de un programa de tutoreo

--- src/api/services/TutoringProgramService.ts
+++ src/api/services/TutoringProgramService.ts
@@ -1,13 +1,18 @@
 import { API_CONFIG } from "../../config/api";
-import type { Form } from "../../types/Types";
+import type { Form, TutoringProgramPayload } from "../../types/Types";
 import { apiClient } from "../client";
 
 class TutoringProgramService {
   private getFormQuestionsEndpoint =
     API_CONFIG.endpoint.tutoringProgram.getFormQuestion;
+  private createEndpoint = API_CONFIG.endpoint.tutoringProgram.create;
 
   async getFormQuestion(): Promise<Form[]> {
     return apiClient.get<Form[]>(this.getFormQuestionsEndpoint);
+  }
+
+  async create(data: TutoringProgramPayload): Promise<any> {
+    return apiClient.post<any>(this.createEndpoint, data);
   }
 }
 
