d20ad7e103271d770c323c087eb6f1e5fe68b53d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-22 08:48:58 -0600

    feat(service): metodo GET por Id

--- src/api/services/TutoringProgramService.ts
+++ src/api/services/TutoringProgramService.ts
@@ -1,14 +1,23 @@
 import { API_CONFIG } from "../../config/api";
-import type { Form, TutoringProgramPayload } from "../../types/Types";
+import type {
+  Form,
+  TutoringProgramList,
+  TutoringProgramPayload,
+} from "../../types/Types";
 import { apiClient } from "../client";
 
 class TutoringProgramService {
   private getFormQuestionsEndpoint =
     API_CONFIG.endpoint.tutoringProgram.getFormQuestion;
+  private getByIdEndpoint = API_CONFIG.endpoint.tutoringProgram.getById;
   private createEndpoint = API_CONFIG.endpoint.tutoringProgram.create;
 
   async getFormQuestion(): Promise<Form[]> {
     return apiClient.get<Form[]>(this.getFormQuestionsEndpoint);
+  }
+
+  async getById(id: number): Promise<TutoringProgramList> {
+    return apiClient.get<TutoringProgramList>(`${this.getByIdEndpoint}${id}`);
   }
 
   async create(data: TutoringProgramPayload): Promise<any> {
