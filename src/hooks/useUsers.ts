c1a796aa68592cc8af868e05f0fd1eb7eb8a8d22
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-28 11:32:08 -0600

    feat(hook): registro de usuario

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,1 +1,41 @@
-export const useUsers = () => {};
+import { useCallback, useState } from "react";
+import { usersService } from "../api/services/UsersService";
+import { toast } from "sonner";
+
+export const useUsers = () => {
+  const [isCreating, setIsCreating] = useState<boolean>(false);
+
+  const createUser = useCallback(
+    async (payrollNumber: string, password: string, roleId: number) => {
+      setIsCreating(true);
+
+      try {
+        await usersService.create(payrollNumber, password, roleId);
+
+        toast.success("Usuario registrado", {
+          description: `El número de nómina ${payrollNumber} ahora tiene acceso al sistema.`,
+        });
+      } catch (error: any) {
+        console.error("Error al registrar usuario: ", error);
+
+        const errorMessage =
+          error.response?.data?.message ||
+          "Ocurrio un error al intentar crear el usuario.";
+
+        toast.error("Error de registro", {
+          description: errorMessage,
+        });
+
+        return false;
+      } finally {
+        setIsCreating(false);
+      }
+    },
+    [],
+  );
+
+  return {
+    isCreating,
+    createUser,
+  };
+};
