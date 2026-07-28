8ee4267520edd313cf48f7239c18b1432a57b4d7
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-28 10:17:27 -0600

    feat(service): metodo create

--- src/api/services/UsersService.ts
+++ src/api/services/UsersService.ts
@@ -1,3 +1,20 @@
-class UsersService {}
+import { API_CONFIG } from "../../config/api";
+import { apiClient } from "../client";
+
+class UsersService {
+  private createEndpoint = API_CONFIG.endpoint.users.create;
+
+  async create(
+    payrollNumber: string,
+    password: string,
+    roleId: number,
+  ): Promise<any> {
+    return apiClient.post<any>(this.createEndpoint, {
+      payrollNumber,
+      password,
+      roleId,
+    });
+  }
+}
 
 export const usersService = new UsersService();
