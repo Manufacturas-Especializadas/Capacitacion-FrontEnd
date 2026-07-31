9877c145c6da02046fdbe237f2cede645f56b383
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-31 08:17:43 -0600

    refactor(component): se adapto el HTML para que acepte la funcion update

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,5 +1,14 @@
 import { useEffect, useState, type SyntheticEvent } from "react";
-import { UserPlus, Hash, Lock, ShieldCheck, Loader2 } from "lucide-react";
+import {
+  UserPlus,
+  Hash,
+  Lock,
+  ShieldCheck,
+  Loader2,
+  Edit,
+  ToggleLeft,
+  ToggleRight,
+} from "lucide-react";
 import { useUsers } from "../../hooks/useUsers";
 import { useLocation, useNavigate } from "react-router-dom";
 import type { Users } from "../../types/Types";
@@ -69,13 +78,15 @@ export const Register = () => {
             className="w-16 h-16 bg-blue-50 text-blue-600 rounded-full flex items-center 
             justify-center mb-4 border border-blue-100"
           >
-            <UserPlus size={32} />
+            {isEditMode ? <Edit size={32} /> : <UserPlus size={32} />}
           </div>
           <h2 className="text-2xl font-bold text-gray-800">
-            Registrar Usuario
+            {isEditMode ? "Editar Usuario" : "Registrar Usuario"}
           </h2>
           <p className="text-gray-500 text-sm mt-1">
-            Crea un nuevo acceso al portal asignando un rol
+            {isEditMode
+              ? `Modificando acceso de nómina: ${userToEdit.payrollNumber}`
+              : "Crea un nuevo acceso al portal asignando un rol"}
           </p>
         </div>
 
@@ -96,37 +107,38 @@ export const Register = () => {
                 required
                 value={payrollNumber}
                 onChange={(e) => setPayrollNumber(e.target.value)}
-                placeholder="Ej. 6294"
                 className="block w-full pl-10 pr-4 py-2.5 bg-gray-50 border border-gray-200 
                 rounded-xl text-gray-800 focus:bg-white focus:ring-2 focus:ring-blue-600 
-                focus:border-transparent transition-all outline-none"
+                outline-none"
               />
             </div>
           </div>
 
-          <div>
-            <label className="block text-sm font-semibold text-gray-700 mb-1.5">
-              Contraseña
-            </label>
-            <div className="relative">
-              <div
-                className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none 
-                text-gray-400"
-              >
-                <Lock size={18} />
+          {!isEditMode && (
+            <div>
+              <label className="block text-sm font-semibold text-gray-700 mb-1.5">
+                Contraseña
+              </label>
+              <div className="relative">
+                <div
+                  className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none 
+                  text-gray-400"
+                >
+                  <Lock size={18} />
+                </div>
+                <input
+                  type="password"
+                  required={!isEditMode}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="••••••••"
+                  className="block w-full pl-10 pr-4 py-2.5 bg-gray-50 border border-gray-200 
+                  rounded-xl text-gray-800 focus:bg-white focus:ring-2 focus:ring-blue-600 
+                  outline-none"
+                />
               </div>
-              <input
-                type="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="••••••••"
-                className="block w-full pl-10 pr-4 py-2.5 bg-gray-50 border border-gray-200 
-                rounded-xl text-gray-800 focus:bg-white focus:ring-2 focus:ring-blue-600 
-                focus:border-transparent transition-all outline-none"
-              />
             </div>
-          </div>
+          )}
 
           <div>
             <label className="block text-sm font-semibold text-gray-700 mb-1.5">
@@ -145,8 +157,7 @@ export const Register = () => {
                 onChange={(e) => setRoleId(Number(e.target.value))}
                 className="block w-full pl-10 pr-4 py-2.5 bg-gray-50 border border-gray-200 
                 rounded-xl text-gray-800 focus:bg-white focus:ring-2 focus:ring-blue-600 
-                focus:border-transparent transition-all outline-none appearance-none 
-                hover:cursor-pointer"
+                outline-none hover:cursor-pointer"
               >
                 <option value={0} disabled>
                   Seleccione un rol...
@@ -160,31 +171,72 @@ export const Register = () => {
             </div>
           </div>
 
-          <button
-            type="submit"
-            disabled={isCreating || roleId === 0}
-            className="w-full mt-6 flex items-center justify-center gap-2 bg-blue-600 
-            hover:bg-blue-500 text-white px-8 py-3 rounded-xl font-semibold transition-colors 
-            shadow-sm hover:cursor-pointer disabled:opacity-70 disabled:cursor-not-allowed"
-          >
-            {isCreating ? (
-              <>
-                <Loader2 size={20} className="animate-spin" />
-                <span>Registrando...</span>
-              </>
-            ) : (
-              <span>Confirmar Registro</span>
-            )}
-          </button>
-          <button
-            type="button"
-            onClick={() => navigate("/usuarios")}
-            className="w-full mt-6 flex items-center justify-center gap-2 bg-gray-600 
-            hover:bg-gray-500 text-white px-8 py-3 rounded-xl font-semibold transition-colors 
-            shadow-sm hover:cursor-pointer disabled:opacity-70 disabled:cursor-not-allowed"
-          >
-            Volver
-          </button>
+          {isEditMode && (
+            <div
+              className="flex items-center justify-between p-3 bg-gray-50 border 
+              border-gray-200 rounded-xl"
+            >
+              <div>
+                <label className="block text-sm font-semibold text-gray-700">
+                  Estatus del Usuario
+                </label>
+                <span className="text-xs text-gray-500">
+                  {isActive
+                    ? "Tiene acceso al sistema"
+                    : "Acceso revocado temporalmente"}
+                </span>
+              </div>
+              <button
+                type="button"
+                onClick={() => setIsActive(!isActive)}
+                className={`transition-colors duration-200 hover:cursor-pointer ${
+                  isActive ? "text-green-500" : "text-gray-400"
+                }`}
+              >
+                {isActive ? (
+                  <ToggleRight size={36} />
+                ) : (
+                  <ToggleLeft size={36} />
+                )}
+              </button>
+            </div>
+          )}
+
+          <div className="pt-2 space-y-3">
+            <button
+              type="submit"
+              disabled={
+                isEditMode
+                  ? isUpdating || roleId === 0
+                  : isCreating || roleId === 0
+              }
+              className="w-full flex items-center justify-center gap-2 bg-blue-600 
+              hover:bg-blue-500 text-white px-8 py-3 rounded-xl font-semibold 
+              transition-colors shadow-sm disabled:opacity-70 disabled:cursor-not-allowed
+              hover:cursor-pointer"
+            >
+              {isCreating || isUpdating ? (
+                <>
+                  <Loader2 size={20} className="animate-spin" />
+                  <span>{isEditMode ? "Guardando..." : "Registrando..."}</span>
+                </>
+              ) : (
+                <span>
+                  {isEditMode ? "Guardar Cambios" : "Confirmar Registro"}
+                </span>
+              )}
+            </button>
+
+            <button
+              type="button"
+              onClick={() => navigate(-1)}
+              className="w-full flex items-center justify-center gap-2 bg-gray-100 
+              hover:bg-gray-200 text-gray-700 px-8 py-3 rounded-xl font-semibold 
+              transition-colors hover:cursor-pointer"
+            >
+              Cancelar
+            </button>
+          </div>
         </form>
       </div>
     </div>
